2e5735f70205d48725f60bceb42466cd53e32a7a
vigneshat-Git <vickydivya179@gmail.com>
2026-03-25 01:13:52 +0530

    vercel-frontend

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,7 +6,7 @@ export const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: parseInt(process.env.PORT || '5000', 10),
   API_BASE_URL: process.env.API_BASE_URL || 'https://peaple-production.up.railway.app',
-  FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:8080",
+  FRONTEND_URL: process.env.FRONTEND_URL || "https://peaple-rho.vercel.app",
 
   // Database
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:ByOiarfBLYvjjevBrVuMqTQKzCVjeoFQ@ballast.proxy.rlwy.net:58471/railway',
