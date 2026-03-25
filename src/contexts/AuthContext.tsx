c5598fd6cde1da0c82816670773c5cd250b406c6
vigneshat-Git <vickydivya179@gmail.com>
2026-03-25 10:17:47 +0530

    register/api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -24,7 +24,7 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | null>(null);
 
 //const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://peaple-production.up.railway.app";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://peaple-production.up.railway.app/api";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
